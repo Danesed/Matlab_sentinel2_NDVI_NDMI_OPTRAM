4460eb4f753cc7fd7415944a75251fa7bb5a292d
Danesed <danese.d@live.it>
2021-10-05 16:27:07 +0200

    riformattazione 1

--- script_studio_cillarese.m
+++ script_studio_cillarese.m
@@ -13,7 +13,7 @@ for k = 1 : length(B04files)
     Datelist{k} = baseFileName(8:15);
 end
 fprintf('\n')
-
+ 
 for k = 1 : length(B04files)
     fprintf('Now clipping %s\n',B04files(k).name);
     B04list{k} = B04list{k}(8980:10980,4000:6000);
@@ -26,9 +26,9 @@ for k = 1 : length(B04files)
     B04list_95{k} = prctile(reshape(B04list{k},1, numel(B04list{k})),95);
 end
 fprintf('\n');
-
+ 
 %B08 FILES
-
+ 
 B08Pattern = fullfile('/Users/danilod/Documents/MATLAB/TVA/Cillarese immagini/test/B08S/*.jp2'); % Change to whatever pattern you need.
 B08files = dir(B08Pattern);
 for k = 1 : length(B08files)
@@ -41,8 +41,8 @@ for k = 1 : length(B08files)
     %imshow(imageArray);  % Display image.
 end
 fprintf('\n');
-
-
+ 
+ 
 for k = 1 : length(B08files)
     fprintf('Now clipping and converting %s\n',B08files(k).name);
     B08list{k} = B08list{k}(8980:10980,4000:6000);
@@ -52,9 +52,9 @@ for k = 1 : length(B08files)
     B08list_95{k} = prctile(reshape(B08list{k},1, numel(B08list{k})),95);
 end
 fprintf('\n')
-
+ 
 %B11 FILES
-
+ 
 B11Pattern = fullfile('/Users/danilod/Documents/MATLAB/TVA/Cillarese immagini/test/B11S/*.jp2'); % Change to whatever pattern you need.
 B11files = dir(B11Pattern);
 for k = 1 : length(B11files)
@@ -67,7 +67,7 @@ for k = 1 : length(B11files)
     %imshow(imageArray);  % Display image.
 end
 fprintf('\n')
-
+ 
 for k = 1 : length(B11files)
     fprintf('Now clipping and converting %s\n',B11files(k).name);
     B11list{k} = B11list{k}(8980:10980,4000:6000);
@@ -76,25 +76,25 @@ for k = 1 : length(B11files)
     B11list_95{k} = prctile(reshape(B11list{k},1, numel(B11list{k})),95);
 end
 fprintf('\n')
-
-
+ 
+ 
 %% calcolo ndvi
-
+ 
 for k = 1 : length(B04files)
     fprintf('Now calculating NDVI %s\n',B04files(k).name);
     NDVI_list{k} = (B08list{k}-B04list{k})./(B08list{k}+B04list{k});
 end
 fprintf('\n')
-
+ 
 %calcolo ndmi
-
+ 
 for k = 1 : length(B11files)
     fprintf('Now calculating NDMI %s\n',B11files(k).name);
     NDMI_list{k} = (B08list{k}-B11list{k})./(B08list{k}+B11list{k});
 end
-
+ 
 %% filtro il mare, usando banda B08
-
+ 
 for k = 1 : length(B08files)
     fprintf('Now masking out water \n');
     Sea_Mask_list{k} = (B08list{k}>1400);
@@ -102,15 +102,15 @@ for k = 1 : length(B08files)
     NDMI_list{k} = (NDMI_list{k}.*Sea_Mask_list{k});
 end
 fprintf('\n')
-
-
+ 
+ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+ 
 %% montage(NDVI_list);
-
-
+ 
+ 
 fprintf('Now creating montage of NDVI \n');
-
+ 
 for k = 1 : length(B11files)
     
 figure('Position',[100 100 1650 450])
@@ -120,30 +120,30 @@ title('650 nm')
 colormap(A1,'Gray'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A2 = axes('Position',[0.325 0.1 0.4 0.8]);
 imagesc(B08list{k},[B08list_05{k} B08list_95{k}])
 title('850 nm')
 colormap(A2,'Gray'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDVI_list{k},[0 1])
 title(B04files(k).date)
 colormap(A3,'turbo'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['1_NDVI_calculation_',num2str(k),'.png']; 
 saveas(gca,temp);
-
+ 
 end
-
-
+ 
+ 
 %montage(NDMI_list);
-
+ 
 fprintf('Now creating montage of NDMI \n');
 for k = 1 : length(B11files)
     
@@ -154,59 +154,59 @@ title('1610 nm')
 colormap(A1,'Gray'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A2 = axes('Position',[0.325 0.1 0.4 0.8]);
 imagesc(B08list{k},[B08list_05{k} B08list_95{k}])
 title('850 nm')
 colormap(A2,'Gray'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDMI_list{k},[-1 1])
 title(B04files(k).date)
 colormap(A3,'parula'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['2_NDMI_calculation_',num2str(k),'.png']; 
 saveas(gca,temp);
-
+ 
 end
-
-
-
-
+ 
+ 
+ 
+ 
 %% CROPPING ON CROPS
-
+ 
 for k = 1 : length(B04files)
     fprintf('Now clipping NDVI and NDMI on sample crops \n');
     NDVI_list_crop{k} = NDVI_list{k}(1200:1550,450:750);
     NDMI_list_crop{k} = NDMI_list{k}(1200:1550,450:750);
 end
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
 %% segmentazione con centroidi
-
+ 
 %%% FOR media centroidi 
-
-
+ 
+ 
 for k = 1 : length(B04files)
 fprintf('Now masking and smoothing crops with NDVI over 0.5 \n');
 NDVI_list_crop_over05_logic{k} = (NDVI_list_crop{k})>0.5;
 NDVI_list_crop_over05{k} = NDVI_list_crop_over05_logic{k} .* NDVI_list_crop{k};
-
+ 
 %crea maschera oltre 5 pixel
 se = strel('square',5);
 NDVI_list_crop_over05_smooth{k} = imopen(NDVI_list_crop_over05_logic{k},se);
 NDVI_list_crop_over05_logic_smooth{k} = imopen(NDVI_list_crop_over05_logic{k},se);
 end
-
+ 
 %%%media centroidi
 for k = 1 : length(B04files)
     figure
@@ -214,9 +214,9 @@ s = regionprops(NDVI_list_crop_over05_logic_smooth{k},NDVI_list_crop_over05{k},{
 props_list{k}=s;
 numObj{k} = numel(s);
 imshow(NDVI_list_crop_over05{k})
-
+ 
 %imshow(labeloverlay(Campo_crop_esempio,BWfinal))
-
+ 
 title('Mean value of NDVI on sample crops')
 hold on
 for j = 1:numObj{k}
@@ -230,7 +230,7 @@ for j = 1:numObj{k}
 end
 hold off
 end
-
+ 
 %%%mostra media per ogni campo
 for k = 1 : length(B04files)
 figure
@@ -239,14 +239,14 @@ xlabel('Region Label Number')
 ylabel('Man value of NDVI on sample crops')
 end
 %%% fine media con centroidi
-
+ 
 %% extra histogram
 % plot histogramma media, crop ndmi, ndmi completo
 %{
 for k = 1 : length(B04files)
     
 figure('Position',[100 100 1650 450])
-
+ 
 A1 = axes('Position',[0.05 0.1 0.4 0.8]);
 [counts, grayLevels] = imhist(NDVI_list_crop{k}, 300);
 bar(grayLevels, counts, 'BarWidth', 0.95);
@@ -256,28 +256,28 @@ colormap(A1,'parula')
 set(gca,'FontSize',14)
 axis auto tight
 colorbar('location','southoutside','Ticks',[],'TickLabels',{''});
-
-
+ 
+ 
 A2 = axes('Position',[0.375 0.1 0.4 0.8]);
 imagesc(NDMI_list_crop{k},[-1 1])
 title(NDMI_mean(k))
 colormap(A2,'parula')
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDMI_list{k},[-1 1])
 title(B04files(k).date)
 colormap(A3,'parula'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['6_NDMI_hist_',num2str(k),'.png']; 
 saveas(gca,temp);
 end
 %}
-
+ 
 %% media vecchia
 %{
 %mean value of ndvi for cropped area
@@ -286,12 +286,12 @@ for k = 1 : length(B04files)
     NDVI_mean{k} = mean(mean(NDVI_list_crop{k}));
     NDMI_mean{k} = mean(mean(NDMI_list_crop{k})); 
 end
-
+ 
 %grafico andamento media
 Y = cell2mat(NDVI_mean);
 X = datetime(Datelist, 'InputFormat', 'yyyyMMdd');
-
-
+ 
+ 
 % plot grafico media, crop ndvi, ndvi completo
 for k = 1 : length(B04files)
     
@@ -302,30 +302,30 @@ title('NDVI mean value over time')
 colormap(A1,'Gray')
 set(gca,'FontSize',14)
 axis auto tight
-
+ 
 A2 = axes('Position',[0.375 0.1 0.4 0.8]);
 imagesc(NDVI_list_crop{k},[0 1])
 title(NDVI_mean(k))
 colormap(A2,'turbo')
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDVI_list{k},[0 1])
 title(B04files(k).date)
 colormap(A3,'turbo'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['3_NDVI_plot_',num2str(k),'.png']; 
 saveas(gca,temp);
 end
-
-
+ 
+ 
 %conversione per plotting
 Y2 = cell2mat(NDMI_mean);
-
+ 
 % plot grafico media, crop ndmi, ndmi completo
 %{
 for k = 1 : length(B04files)
@@ -337,29 +337,29 @@ title('NDMI mean value over time')
 colormap(A1,'Gray')
 set(gca,'FontSize',14)
 axis auto tight
-
+ 
 A2 = axes('Position',[0.375 0.1 0.4 0.8]);
 imagesc(NDMI_list_crop{k},[-1 1])
 title(NDMI_mean(k))
 colormap(A2,'parula')
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDMI_list{k},[-1 1])
 title(B04files(k).date)
 colormap(A3,'parula'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['4_NDMI_plot_',num2str(k),'.png']; 
 saveas(gca,temp);
 end
 %}
-
+ 
 %%%%%%plot con istogramma
-
+ 
 %{
 % histogramma grafico media, crop ndvi, ndvi completo
 for k = 1 : length(B04files)
@@ -372,28 +372,28 @@ colormap(A1,'turbo')
 set(gca,'FontSize',14)
 axis auto tight
 colorbar('location','southoutside','Ticks',[],'TickLabels',{''})
-
+ 
 A2 = axes('Position',[0.375 0.1 0.4 0.8]);
 imagesc(NDVI_list_crop{k},[0 1])
 title(NDVI_mean(k))
 colormap(A2,'turbo')
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDVI_list{k},[0 1])
 title(B04files(k).date)
 colormap(A3,'turbo'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['5_NDVI_hist_',num2str(k),'.png']; 
 saveas(gca,temp);
 end
-
+ 
 %}
-
+ 
 % plot histogramma media, crop ndmi, ndmi completo
 %{
 for k = 1 : length(B04files)
@@ -408,41 +408,30 @@ colormap(A1,'parula')
 set(gca,'FontSize',14)
 axis auto tight
 colorbar('location','southoutside','Ticks',[],'TickLabels',{''})
-
-
+ 
+ 
 A2 = axes('Position',[0.375 0.1 0.4 0.8]);
 imagesc(NDMI_list_crop{k},[-1 1])
 title(NDMI_mean(k))
 colormap(A2,'parula')
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 A3 = axes('Position',[0.625 0.1 0.4 0.8]);
 imagesc(NDMI_list{k},[-1 1])
 title(B04files(k).date)
 colormap(A3,'parula'), colorbar
 set(gca,'FontSize',14)
 axis square tight, axis off
-
+ 
 hold on;
 temp=['6_NDMI_hist_',num2str(k),'.png']; 
 saveas(gca,temp);
 end
 %}
-
+ 
 %}
-
-<<<<<<< HEAD
-%%%mostra media per ogni campo
-for k = 1 : length(B04files)
-figure
-bar(1:numObj{k},[props_list{k}.mean])
-xlabel('Region Label Number')
-ylabel('Man value of NDVI on sample crops')
-end
-
-=======
->>>>>>> a5e9cff... Riformattazione
+ 
 %%%%%
 %% fine
 fprintf('\n ____FINE____\n')
